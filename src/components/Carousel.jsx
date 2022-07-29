a6d76e21affd37c7bdbf1f9a02ea4f0a6fe1d251
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-07-29 04:37:32 +0100

    working on products page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import img1 from "../assets/carousel1.jpg";
 
 const Carousel = ()=>{
   return(
     <>
       <div className="carousel">
-        
+        <img src={img1} alt="" />
       </div>
     </>
   )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import Navbar from "./Navbar.jsx";
 import Carousel from "./Carousel.jsx";
 import test from "../assets/test.png";
+import Pagination from "./Pagination.jsx";
+import { useState } from "react";
 
 const Products = ()=>{
+  const [page , setPage] = useState(2);
+  const [pages , setPages] = useState(7);
+
   return(
     <>
       <section id="products">
@@ -64,11 +69,38 @@ const Products = ()=>{
               </div>
               <button>Add to Cart</button>
             </div>
-
-
-
-            
+            <div className="item">
+              <img src={test} alt="" />
+              <div>
+                <h3>name</h3>
+                <h3>200</h3>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+            <div className="item">
+              <img src={test} alt="" />
+              <div>
+                <h3>name</h3>
+                <h3>200</h3>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+            <div className="item">
+              <img src={test} alt="" />
+              <div>
+                <h3>name</h3>
+                <h3>200</h3>
+              </div>
+              <button>Add to Cart</button>
+            </div>
           </div>
+        </div>
+        <div className="pagination">
+            <Pagination
+              page={page}
+              setPage={setPage}
+              pages={pages}
+            />
         </div>
       </section>
     </>
