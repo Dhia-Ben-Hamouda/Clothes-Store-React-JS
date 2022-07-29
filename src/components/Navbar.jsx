21618eaba8af042011afad3ec0590ede8e9c60ec
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-07-29 02:29:12 +0100

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { FaSearch , FaUser , FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = ()=>{
+  const [search , setSearch ] = useState("");
+
   return(
     <nav>
       <div className="logo">
-        <img src={logo} alt="" />
+        <div className="img">
+          <img src={logo} alt="" />
+        </div>
+        <h1>Clothes Store</h1>
+      </div>
+      <div className="container">
+        <div className="search-container">
+          <FaSearch size="1.25rem" color="#777" />
+          <input
+            placeholder="Search for products"
+            value={search}
+            onChange={(e)=>{setSearch(e.target.value)}}
+          />
+        </div>
+        <div>
+          <a href="#">
+            <FaUser className="icon"/>
+          </a>
+          <a href="#">
+            <FaShoppingCart className="icon cart"/> 
+          </a>
+          <div className="count">
+            0
+          </div>
+        </div>
       </div>
     </nav>
   )
