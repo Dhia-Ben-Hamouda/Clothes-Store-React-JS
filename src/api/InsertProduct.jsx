df561b3db509ee8d4a418fe6dd6ee3767dffe3eb
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-01 11:09:52 +0100

    add product images

--- src/api/InsertProduct.jsx
+++ src/api/InsertProduct.jsx
@@ -1,59 +1,90 @@
 import React from "react";
 import { useState } from "react";
-import { TextField , Button} from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const InsertProduct = ()=>{
-  const [price , setPrice] = useState("");
-  const [gender , setGender] = useState("");
-  const [size , setSize] = useState("");
-  const [color , setColor] = useState("");
+const InsertProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [gender, setGender] = useState("");
+  const [size, setSize] = useState("");
+  const [color, setColor] = useState("");
+  const [picture , setPicture] = useState("");
 
-  async function submitHandler(e)
-  {
+  const theme = createTheme({
+    palette: {
+      primary:{
+        main:"#FB9D12"
+      }
+    }
+  })
+
+  async function submitHandler(e) {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/insertProduct" , {
-      method:"POST",
-      headers:{
-        "content-type":"application/json"
+    const response = await fetch("http://localhost:5000/products/insertProduct", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
       },
       body: JSON.stringify({
-
+        name,
+        price,
+        gender,
+        size,
+        color,
+        picture
       })
     })
-    
+
     const data = await response.json();
+
+    console.log(data);
   }
 
-  return(
+  return (
     <>
-      <form autoComplete="off" onSubmit={submitHandler} style={{display:"flex" , flexDirection:"column" , padding:"1rem"}}>
-        <TextField 
-          value={gender}
-          onChange={(e)=>{setGender(e.target.value)}}
-          label="Gender"
-          style={{marginBottom:"1rem"}}  
-        />
-        <TextField 
-          value={size}
-          onChange={(e)=>{setSize(e.target.value)}}
-          label="Size"
-          style={{marginBottom:"1rem"}}  
-        />
-        <TextField 
-          value={color}
-          onChange={(e)=>{setColor(e.target.value)}}
-          label="Color"
-          style={{marginBottom:"1rem"}}  
-        />
-        <TextField 
-          value={price}
-          onChange={(e)=>{setPrice(e.target.value)}}
-          label="Price"
-          style={{marginBottom:"1rem"}}  
-        />
-        <Button variant="contained" type="submit">Submit</Button>
-      </form>  
+      <ThemeProvider theme={theme}>
+        <form autoComplete="off" onSubmit={submitHandler} style={{ display: "flex", flexDirection: "column", padding: "1rem" }}>
+          <TextField
+            value={name}
+            onChange={(e) => { setName(e.target.value) }}
+            label="Name"
+            style={{ marginBottom: "1rem" }}
+          />
+          <TextField
+            value={gender}
+            onChange={(e) => { setGender(e.target.value) }}
+            label="Gender"
+            style={{ marginBottom: "1rem" }}
+          />
+          <TextField
+            value={size}
+            onChange={(e) => { setSize(e.target.value) }}
+            label="Size"
+            style={{ marginBottom: "1rem" }}
+          />
+          <TextField
+            value={color}
+            onChange={(e) => { setColor(e.target.value) }}
+            label="Color"
+            style={{ marginBottom: "1rem" }}
+          />
+          <TextField
+            value={price}
+            onChange={(e) => { setPrice(e.target.value) }}
+            label="Price"
+            style={{ marginBottom: "1rem" }}
+          />
+          <TextField
+            value={picture}
+            onChange={(e)=>{setPicture(e.target.value)}}
+            label="Picture"
+            style={{marginBottom:"1rem"}}
+          />
+          <Button style={{color:"white"}} variant="contained" type="submit">Submit</Button>
+        </form>
+      </ThemeProvider>
     </>
   )
 }
