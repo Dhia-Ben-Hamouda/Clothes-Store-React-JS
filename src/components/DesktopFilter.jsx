534affc768c7c5c1f32fb8e3c6c08bec17901a5d
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-02 10:55:08 +0100

    working on filtering

---
+++ src/components/DesktopFilter.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Slider, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+
+const DesktopFilter = ({filters , setFilters , priceRange , setPriceRange}) => {
+
+  function handleFilter(e)
+  {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if(e.target.checked)
+    {
+      let tab = filters[name];
+      console.log(tab);
+      tab.push(value);
+      setFilters({...filters , [name] : tab});
+      console.log(filters);
+    }
+    else
+    {
+      let tab = filters[name];
+      console.log(tab);
+      tab = tab.filter((item)=>item !== value);
+      setFilters({...filters , [name] : tab});
+      console.log(filters);
+    }  
+  }
+
+  return (
+    <>
+      <div className="filter-container">
+        <div className="header">
+          Filter by
+        </div>
+        <div className="price">
+          <h1>Price</h1>
+          <div className="slider">
+            <Slider
+              value={priceRange}
+              valueLabelDisplay="auto"
+              min={0}
+              max={500}
+              step={20}
+              onChange={(e, newValue) => { setPriceRange(newValue); console.log(newValue) }}
+            />
+
+          </div>
+          <div className="price-inputs">
+            <div className="low">
+              {
+                priceRange[0] + " $"
+              }
+            </div>
+            <div className="high">
+              {
+                priceRange[1] + " $"
+              }
+            </div>
+          </div>
+        </div>
+        <div className="gender">
+          <h1>Gender</h1>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="gender" value="male" />}
+              label="Male"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="gender" value="female" />}
+              label="Female"
+            />
+          </FormGroup>
+        </div>
+        <div className="size">
+          <h1>Size</h1>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="size" value="S" />}
+              label="S"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="size" value="M" />}
+              label="M"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="size" value="L" />}
+              label="L"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="size" value="XL" />}
+              label="XL"
+            />
+          </FormGroup>
+        </div>
+        <div className="color">
+          <h1>Color</h1>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="color" value="black" />}
+              label="Black"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="color" value="white" />}
+              label="White"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="color" value="red" />}
+              label="Red"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="color" value="blue" />}
+              label="Blue"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="color" value="yellow" />}
+              label="Yellow"
+            />
+            <FormControlLabel
+              control={<Checkbox onChange={handleFilter} name="color" value="green" />}
+              label="Green"
+            />
+          </FormGroup>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DesktopFilter;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,11 +3,10 @@ import Navbar from "./Navbar.jsx";
 import Carousel from "./Carousel.jsx";
 import Pagination from "./Pagination.jsx";
 import { useState } from "react";
-import { Slider, FormGroup, FormControlLabel, Checkbox } from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect } from "react";
 import Product from "./Product.jsx";
-
+import DesktopFilter from "./DesktopFilter.jsx";
 
 const theme = createTheme({
   palette: {
@@ -18,19 +17,25 @@ const theme = createTheme({
 })
 
 const Products = () => {
-  const [page, setPage] = useState(2);
-  const [pages, setPages] = useState(7);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(5);
   const [priceRange, setPriceRange] = useState([0, 500]);
   const [products, setProducts] = useState([]);
+  const [filters , setFilters ] = useState({
+    gender:[],
+    size:[],
+    color:[]
+  })
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://clothes-store-react-js.herokuapp.com/products/getAllProducts");
+      const response = await fetch(`https://clothes-store-react-js.herokuapp.com/products/getAllProducts?page=${page}?&filters=${JSON.stringify(filters)}`);
       const data = await response.json();
+
       setProducts(data);
     }
     fetchData();
-  }, []);
+  }, [page , filters ]);
 
   return (
     <>
@@ -39,101 +44,12 @@ const Products = () => {
         <Carousel />
         <ThemeProvider theme={theme}>
           <div className="wrapper">
-            <div className="filter-container">
-              <div className="header">
-                Filter by
-              </div>
-              <div className="price">
-                <h1>Price</h1>
-                <div className="slider">
-
-                  <Slider
-                    value={priceRange}
-                    valueLabelDisplay="auto"
-                    min={0}
-                    max={500}
-                    step={20}
-                    onChange={(e, newValue) => { setPriceRange(newValue); console.log(newValue) }}
-                  />
-
-                </div>
-                <div className="price-inputs">
-                  <div className="low">
-                    {
-                      priceRange[0] + " $"
-                    }
-                  </div>
-                  <div className="high">
-                    {
-                      priceRange[1] + " $"
-                    }
-                  </div>
-                </div>
-              </div>
-              <div className="gender">
-                <h1>Gender</h1>
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Male"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Female"
-                  />
-                </FormGroup>
-              </div>
-              <div className="size">
-                <h1>Size</h1>
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="S"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="M"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="L"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="XL"
-                  />
-                </FormGroup>
-              </div>
-              <div className="color">
-                <h1>Color</h1>
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Black"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="White"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Red"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Blue"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Yellow"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Green"
-                  />
-                </FormGroup>
-              </div>
-            </div>
+            <DesktopFilter
+              filters={filters}
+              setFilters={setFilters}
+              priceRange={priceRange}
+              setPriceRange={setPriceRange}
+            />
             <div className="product-container">
               {
                 products.map((product, index) => {
