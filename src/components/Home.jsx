39c1393007a7168e3522317464a9adb5ff10d25c
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-04 08:13:07 +0100

    more work on responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
 import shirt from "../images/shirt.png";
-import { FaSearch } from "react-icons/fa";
 
 const Home = () => {
   return (
     <section id="home">
       <Navbar />
-      <div className="mobile-search">
-        <FaSearch color="#777"/>
-        <input
-          placeholder="Search for products"
-        />
-      </div>
       <div className="wrapper">
         <div className="inner-wrapper">
           <div className="text">
