460e6f174ec973ffd073963641379b32446b04c6
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-09 14:34:39 +0100

    create carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,13 +4,37 @@ import { useState } from "react";
 import img1 from "../images/carousel1.jpg";
 import img2 from "../images/carousel2.jpg";
 import img3 from "../images/carousel3.jpg";
+import Carousell from 'react-bootstrap/Carousel';
+import "../styles/_bootstrap.scss";
 
-const Carousel = ()=>{
+const Carousel = () => {
 
-  return(
+  return (
     <>
-      <div className="carousel">
-        <img src={img1} alt=""/>
+      <div className="local-bootstrap">
+        <Carousell interval={3000}>
+          <Carousell.Item>
+            <img
+              className="d-block w-100"
+              src={img1}
+              alt="First slide"
+            />
+          </Carousell.Item>
+          <Carousell.Item>
+            <img
+              className="d-block w-100"
+              src={img2}
+              alt="Second slide"
+            />
+          </Carousell.Item>
+          <Carousell.Item>
+            <img
+              className="d-block w-100"
+              src={img3}
+              alt="Third slide"
+            />
+          </Carousell.Item>
+        </Carousell>
       </div>
     </>
   )
